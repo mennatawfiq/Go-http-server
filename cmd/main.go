03e55b16c865f2a76f581e85473d542581a61e18
menna <twmnh195@gmail.com>
2025-04-28 01:45:01 +0300

    feat: extracting path

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,45 @@
 package main
 
 import (
-	"errors"
-	"io"
-	"log"
-	"net/http"
-	"time"
+	"fmt"
+	"net"
+	"os"
+	"strings"
 )
 
-func timeElapsed(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		f(w, r)
-		end := time.Now()
-		log.Printf("request took: %v", end.Sub(start))
-	}
-}
-
-func handleStatus(w http.ResponseWriter, r *http.Request) {
-	log.Print("handling status request")
-	io.WriteString(w, "Server is running\n")
-}
-
 func main() {
-	http.HandleFunc("/status", timeElapsed(handleStatus))
 
-	err := http.ListenAndServe(":1220", nil)
-	if err != nil && errors.Is(err, http.ErrServerClosed) {
-		log.Print("closing server")
-	} else {
-		log.Fatal(err)
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
 	}
+
+	defer l.Close()
+
+	conn, er := l.Accept()
+	if er != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	req := string(buf)
+	lines := strings.Split(req, "\r\n")
+	path := strings.Split(lines[0], " ")[1]
+	res := ""
+	if path == "/" {
+		res = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	conn.Write([]byte(res))
+
 }
